4ebcacd779abe868ca157f984105cb17ae67dd11
DiouMedeiros <diou.182@gmail.com>
2019-07-26 00:06:48 -0300

    Implementado Styled Components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-function Main() {
-    return <h1>Main</h1>;
+import { Container, Form, SubmitButton } from './styles';
+
+class Main extends Component {
+    state = {
+        newRepo: '',
+    };
+
+    handleInputChange = e => {
+        this.setState({ newRepo: e.target.value });
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state.newRepo);
+    }
+
+    render() {
+        const { newRepo } = this.state;
+
+        return (
+            <Container>
+                <h1>
+                    <FaGithubAlt />
+                    Repositórios
+                </h1>
+
+                <Form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Adicionar repositórios"
+                        value={newRepo}
+                        onChange={this.handleInputChange}
+                    />
+                    <SubmitButton>
+                        <FaPlus color="#fff" size={14} />
+                    </SubmitButton>
+                </Form>
+            </Container>
+        );
+    }
 }
 
 export default Main;
